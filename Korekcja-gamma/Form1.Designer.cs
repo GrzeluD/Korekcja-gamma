f02a36a8ea41cd8434bc43dd68dd1739d2682042
GrzeluD <grzeluud@gmail.com>
2023-12-13 14:24:34 +0100

    Created GUI and save color of all pixels in loaded image to file

--- Korekcja-gamma/Form1.Designer.cs
+++ Korekcja-gamma/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Korekcja_gamma
 {
-    partial class Form1
+    partial class Gamma
     {
         /// <summary>
         /// Wymagana zmienna projektanta.
@@ -28,13 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Gamma));
+            this.loadImageButton = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.transparencyTrackBar = new System.Windows.Forms.TrackBar();
+            this.processImageButton = new System.Windows.Forms.Button();
+            this.transparencyLabel = new System.Windows.Forms.Label();
+            this.dropDown = new System.Windows.Forms.ComboBox();
+            this.threadsLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.transparencyTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // loadImageButton
+            // 
+            resources.ApplyResources(this.loadImageButton, "loadImageButton");
+            this.loadImageButton.Name = "loadImageButton";
+            this.loadImageButton.UseVisualStyleBackColor = true;
+            this.loadImageButton.Click += new System.EventHandler(this.loadImageButton_Click);
+            // 
+            // pictureBox
+            // 
+            resources.ApplyResources(this.pictureBox, "pictureBox");
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.TabStop = false;
+            // 
+            // transparencyTrackBar
+            // 
+            resources.ApplyResources(this.transparencyTrackBar, "transparencyTrackBar");
+            this.transparencyTrackBar.Maximum = 100;
+            this.transparencyTrackBar.Minimum = -100;
+            this.transparencyTrackBar.Name = "transparencyTrackBar";
+            this.transparencyTrackBar.Value = 0;
+            this.transparencyTrackBar.Scroll += new System.EventHandler(this.transparencyTrackBar_Scroll);
+            // 
+            // processImageButton
+            // 
+            resources.ApplyResources(this.processImageButton, "processImageButton");
+            this.processImageButton.Name = "processImageButton";
+            this.processImageButton.UseVisualStyleBackColor = true;
+            this.processImageButton.Click += new System.EventHandler(this.processImageButton_Click);
+            // 
+            // transparencyLabel
+            // 
+            resources.ApplyResources(this.transparencyLabel, "transparencyLabel");
+            this.transparencyLabel.Name = "transparencyLabel";
+            // 
+            // dropDown
+            // 
+            this.dropDown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.dropDown.FormattingEnabled = true;
+            resources.ApplyResources(this.dropDown, "dropDown");
+            this.dropDown.Name = "dropDown";
+            this.dropDown.Sorted = true;
+            // 
+            // threadsLabel
+            // 
+            resources.ApplyResources(this.threadsLabel, "threadsLabel");
+            this.threadsLabel.Name = "threadsLabel";
+            // 
+            // Gamma
+            // 
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.threadsLabel);
+            this.Controls.Add(this.dropDown);
+            this.Controls.Add(this.transparencyLabel);
+            this.Controls.Add(this.processImageButton);
+            this.Controls.Add(this.transparencyTrackBar);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.loadImageButton);
+            this.Name = "Gamma";
+            this.Load += new System.EventHandler(this.Gamma_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.transparencyTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button loadImageButton;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TrackBar transparencyTrackBar;
+        private System.Windows.Forms.Button processImageButton;
+        private System.Windows.Forms.Label transparencyLabel;
+        private System.Windows.Forms.ComboBox dropDown;
+        private System.Windows.Forms.Label threadsLabel;
     }
 }
 
--- Korekcja-gamma/Form1.cs
+++ Korekcja-gamma/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,89 @@ using System.Windows.Forms;
 
 namespace Korekcja_gamma
 {
-    public partial class Form1 : Form
+    public partial class Gamma : Form
     {
-        public Form1()
+
+        private Bitmap originalBitmap;
+
+        public Gamma()
         {
             InitializeComponent();
+        }
+
+        private void CheckButtonEnabledState()
+        {
+            processImageButton.Enabled = originalBitmap != null && transparencyTrackBar.Value != 0;
+        }
+
+        private void loadImageButton_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Image Files|*.bmp;*.jpg;*.jpeg;*.png;*.gif;*.tif;*.tiff|All files (*.*)|*.*";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox.ImageLocation = openFileDialog.FileName;
+                    originalBitmap = new Bitmap(openFileDialog.FileName);
+                    pictureBox.Image = originalBitmap;
+                    CheckButtonEnabledState();
+                }
+            }
+        }
+
+        private void transparencyTrackBar_Scroll(object sender, EventArgs e)
+        {
+            int transparencyValue = transparencyTrackBar.Value;
+            transparencyLabel.Text = $"{transparencyValue}";
+            CheckButtonEnabledState();
+        }
+
+        private void processImageButton_Click(object sender, EventArgs e)
+        {
+            int width = originalBitmap.Width;
+            int height = originalBitmap.Height;
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text Files|*.txt";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Utwórz StreamWriter, aby zapisać informacje do pliku
+                using (StreamWriter writer = new StreamWriter(saveFileDialog.FileName))
+                {
+                    // Przejdź przez wszystkie piksele obrazu
+                    for (int y = 0; y < height; y++)
+                    {
+                        for (int x = 0; x < width; x++)
+                        {
+                            // Pobierz kolor piksela
+                            Color pixelColor = originalBitmap.GetPixel(x, y);
+
+                            // Zapisz informacje o pikselu do pliku
+                            writer.WriteLine($"Pixel at ({x}, {y}): R={pixelColor.R}, G={pixelColor.G}, B={pixelColor.B}");
+                        }
+                    }
+                }
+
+                MessageBox.Show($"Pixel information saved to {saveFileDialog.FileName}");
+            }
+        }
+
+        private void Gamma_Load(object sender, EventArgs e)
+        {
+            int maxThreads = Environment.ProcessorCount;
+
+            for (int i = 1; i <= maxThreads; i++)
+            {
+                dropDown.Items.Add(i);
+            }
+
+            dropDown.SelectedIndex = 0;
+        }
+
+        private void transparencyThreadsComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedThreads = Convert.ToInt32(dropDown.SelectedItem);
         }
     }
 }
