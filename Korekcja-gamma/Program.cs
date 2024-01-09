a92c307767031f56131d8d20ef45fdf090f3e07a
GrzeluD <grzeluud@gmail.com>
2024-01-09 19:58:07 +0100

    Revert "Update Program.cs"
    This reverts commit d766b6f5b9933cfc9f514aad46b065266bbdbf0f.

--- Korekcja-gamma/Program.cs
+++ Korekcja-gamma/Program.cs
@@ -8,9 +8,6 @@ namespace Korekcja_gamma
 {
     internal static class Program
     {
-
-        [DllImport(@"C:\Users\alber\Source\Repos\Korekcja-gamma\Korekcja-gamma\x64\Debug\KorekcjaGamma.dll")]
-        static extern int MyProc(int pixel, int gamma);
         /// <summary>
         /// Główny punkt wejścia dla aplikacji.
         /// </summary>
